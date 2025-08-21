5da479c1d274ed8bd89ea4f32f5ae48aa13f861d
Zac <97338659+ZProLegend007@users.noreply.github.com>
2025-08-21 14:44:52 +0800

    Update websocket.rs

--- src/websocket.rs
+++ src/websocket.rs
@@ -33,7 +33,6 @@ impl WebSocketManager {
         let (ws_stream, _) = connect_async(&self.server_url).await?;
         let (mut write, mut read) = ws_stream.split();
         
-        // Send join message
         let join_message = ClientMessage::Join {
             username: self.username.clone(),
             chatname: self.chatname.clone(),
@@ -43,75 +42,30 @@ impl WebSocketManager {
         let join_json = serde_json::to_string(&join_message)?;
         write.send(Message::Text(join_json)).await?;
         
-        let event_sender = self.event_sender.clone();
-        let username = self.username.clone();
+        let _ = self.event_sender.send(AppEvent::Connected);
         
-        // Handle incoming messages
         while let Some(message) = read.next().await {
             match message {
                 Ok(Message::Text(text)) => {
                     if let Ok(server_message) = serde_json::from_str::<ServerMessage>(&text) {
                         match server_message {
-                            ServerMessage::Join { username: joined_user } => {
-                                if joined_user == username {
-                                    let _ = event_sender.send(AppEvent::Connected);
-                                } else {
-                                    let _ = event_sender.send(AppEvent::UserJoined(joined_user));
-                                }
-                            }
-                            ServerMessage::Leave { username: left_user } => {
-                                let _ = event_sender.send(AppEvent::UserLeft(left_user));
-                            }
-                            ServerMessage::Message { username: msg_user, content } => {
-                                let is_server = msg_user.to_lowercase() == "server";
-                                let _ = event_sender.send(AppEvent::MessageReceived {
-                                    username: msg_user,
+                            ServerMessage::Message { username, content } => {
+                                let is_server = username.to_lowercase() == "server";
+                                let _ = self.event_sender.send(AppEvent::MessageReceived {
+                                    username,
                                     content,
                                     is_server,
                                 });
                             }
-                            ServerMessage::Error { message } => {
-                                eprintln!("Server error: {}", message);
-                                let _ = event_sender.send(AppEvent::Disconnected);
-                            }
-                            ServerMessage::AuthFailed { message } => {
-                                eprintln!("Auth failed: {}", message);
-                                let _ = event_sender.send(AppEvent::Disconnected);
-                            }
-                            ServerMessage::ColorShift { color } => {
-                                let _ = event_sender.send(AppEvent::ThemeColorChange(color));
-                            }
-                            ServerMessage::BackgroundShift { color } => {
-                                let _ = event_sender.send(AppEvent::BackgroundColorChange(color));
-                            }
-                            ServerMessage::ChatClear => {
-                                let _ = event_sender.send(AppEvent::ClearMessages);
-                            }
-                            ServerMessage::Kicked { message } => {
-                                let _ = event_sender.send(AppEvent::Kicked(message));
-                            }
                         }
                     }
                 }
-                Ok(Message::Close(_)) => {
-                    let _ = event_sender.send(AppEvent::Disconnected);
-                    break;
-                }
-                Err(e) => {
-                    eprintln!("WebSocket error: {}", e);
-                    let _ = event_sender.send(AppEvent::Disconnected);
-                    break;
-                }
+                Ok(Message::Close(_)) => break,
+                Err(_) => break,
                 _ => {}
             }
         }
         
-        Ok(())
-    }
-    
-    pub async fn send_message(&mut self, _message: ClientMessage) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-        // This would need to be implemented with a proper connection handle
-        // For now, this is a placeholder
         Ok(())
     }
 }
