496d9e29c15da18a429fe9d66f7a341b8503387d
Zac <97338659+ZProLegend007@users.noreply.github.com>
2025-08-21 14:43:25 +0800

    Update app_state.rs

--- src/app_state.rs
+++ src/app_state.rs
@@ -16,20 +16,12 @@ pub enum AppEvent {
         password: String,
     },
     Connected,
-    Disconnected,
     MessageReceived {
         username: String,
         content: String,
         is_server: bool,
     },
     SendMessage(String),
-    ClearMessages,
-    UserJoined(String),
-    UserLeft(String),
-    ThemeColorChange(String),
-    BackgroundColorChange(String),
-    Kicked(String),
-    Quit,
 }
 
 pub struct AppState {
@@ -48,81 +40,4 @@ impl AppState {
             connected: false,
         }
     }
-    
-    pub fn set_screen(&mut self, screen: Screen) {
-        self.screen = screen;
-    }
-    
-    pub fn set_username(&mut self, username: String) {
-        self.username = username;
-    }
-    
-    pub fn set_chatname(&mut self, chatname: String) {
-        self.chatname = chatname;
-    }
-    
-    pub fn set_connected(&mut self, connected: bool) {
-        self.connected = connected;
-    }
-}
-```
-
-## src/message.rs
-
-```rust
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(tag = "type")]
-pub enum ClientMessage {
-    #[serde(rename = "join")]
-    Join {
-        username: String,
-        chatname: String,
-        password: String,
-    },
-    #[serde(rename = "message")]
-    Message {
-        content: String,
-    },
-}
-
-#[derive(Debug, Clone, Deserialize)]
-#[serde(tag = "type")]
-pub enum ServerMessage {
-    #[serde(rename = "join")]
-    Join {
-        username: String,
-    },
-    #[serde(rename = "leave")]
-    Leave {
-        username: String,
-    },
-    #[serde(rename = "message")]
-    Message {
-        username: String,
-        content: String,
-    },
-    #[serde(rename = "error")]
-    Error {
-        message: String,
-    },
-    #[serde(rename = "auth_failed")]
-    AuthFailed {
-        message: String,
-    },
-    #[serde(rename = "colourshift")]
-    ColorShift {
-        color: String,
-    },
-    #[serde(rename = "bgshift")]
-    BackgroundShift {
-        color: String,
-    },
-    #[serde(rename = "chatclear")]
-    ChatClear,
-    #[serde(rename = "kicked")]
-    Kicked {
-        message: String,
-    },
 }
